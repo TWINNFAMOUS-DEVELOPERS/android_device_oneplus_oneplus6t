1a39f703f13169c862f8c589933ff36097ad5adf
TWINNFAMOUS <twinnfamous@gmail.com>
2019-01-03 22:45:33 -0800

    Update omni_oneplus6.mk

--- omni_oneplus6.mk
+++ omni_oneplus6.mk
@@ -1,5 +1,5 @@
 # Copyright (C) 2010 The Android Open Source Project
-#
+#twinnfamous 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -42,23 +42,23 @@ $(call inherit-product, vendor/omni/config/common.mk)
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/oneplus/oneplus6/device.mk)
 
-ALLOW_MISSING_DEPENDENCIES := true
+#ALLOW_MISSING_DEPENDENCIES := true
 
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service
 
 # Discard inherited values and use our own instead.
-PRODUCT_NAME := omni_oneplus6
-PRODUCT_DEVICE := oneplus6
+PRODUCT_NAME := omni_oneplus6T
+PRODUCT_DEVICE := oneplus6T
 PRODUCT_BRAND := OnePlus
 PRODUCT_MANUFACTURER := OnePlus
-PRODUCT_MODEL := ONEPLUS A6003
+PRODUCT_MODEL := ONEPLUS A6013
 
-PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=OnePlus6 PRODUCT_NAME=OnePlus6
+PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=OnePlus6T PRODUCT_NAME=OnePlus6T
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT=OnePlus/OnePlus6/OnePlus6:9/PKQ1.180716.001/1810252315:user/release-keys \
-    PRIVATE_BUILD_DESC="OnePlus6-user 9 PKQ1.180716.001 1810252315 release-keys"
+    BUILD_FINGERPRINT=OnePlus/OnePlus6T/OnePlus6T:9/PKQ1.180716.001/1812260627:user/release-keys \
+    PRIVATE_BUILD_DESC="OnePlus6T-user 9 PKQ1.180716.001 1812260627 release-keys"
 
 PLATFORM_SECURITY_PATCH_OVERRIDE := 2018-11-01
 
