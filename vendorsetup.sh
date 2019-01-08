c133bf1078d920660a62ea47ae08e7f00702e9e6
TWINNFAMOUS <twinnfamous@gmail.com>
2019-01-07 17:30:46 -0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,6 +20,6 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo omni_oneplus6T-user
-add_lunch_combo omni_oneplus6T-userdebug
-add_lunch_combo omni_oneplus6T-eng
+add_lunch_combo lineage_oneplus6T-user
+add_lunch_combo lineage_oneplus6T-userdebug
+add_lunch_combo lineage_oneplus6T-eng
